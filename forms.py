55686813f18debf0d3499a1a0ecc5b62e870c698
Oleh Davymuka <olegdavimuka0@gmail.com>
2022-03-15 01:02:10 +0200

    Add Phone Number Validation

--- forms.py
+++ forms.py
@@ -1,7 +1,9 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, EmailField
-from wtforms.validators import DataRequired, URL, Email
+import phonenumbers
 from flask_ckeditor import CKEditorField
+from flask_wtf import FlaskForm
+from phonenumbers import NumberParseException
+from wtforms import StringField, SubmitField, PasswordField, EmailField
+from wtforms.validators import DataRequired, URL, Email, ValidationError
 
 
 class CreatePostForm(FlaskForm):
@@ -33,6 +35,14 @@ class CommentForm(FlaskForm):
 class EmailForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = EmailField("Email Address", validators=[DataRequired(), Email()])
-    phone = StringField("Phone Number", validators=[DataRequired()])  # TODO: phone number validation
+    phone = StringField("Phone Number", validators=[DataRequired()])
     message = StringField("Message", validators=[DataRequired()])
     submit = SubmitField("Send")
+
+    def validate_phone(self, phone):
+        if len(phone.data) > 15:
+            raise ValidationError('Too long.')
+        try:
+            phonenumbers.parse(phone.data)
+        except NumberParseException:
+            raise ValidationError('Invalid phone number.')
